5ac5ea3f16597aabe50ab2187787bdf41ca3d9c5
tum-piyapong <tum-zx@hotmail.com>
2026-02-28 00:17:30 +0700

    line-webhook.js

--- netlify/functions/line-webhook.js
+++ netlify/functions/line-webhook.js
@@ -1,4 +1,11 @@
 exports.handler = async (event) => {
+  if (!event.body) {
+    return {
+      statusCode: 200,
+      body: "Webhook is working",
+    };
+  }
+
   const body = JSON.parse(event.body);
   console.log("Received:", body);
 
